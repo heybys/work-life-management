924e50fac82e5f4e17e86316fb5b79f7af4de44b
heybys <byn0174@gmail.com>
2024-03-04 01:30:41 +0900

    init project

---
+++ src/main/java/com/smilegate/workflexmanagement/WorkFlexManagementApplication.java
@@ -0,0 +1,13 @@
+package com.smilegate.workflexmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlexManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFlexManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smilegate/workflexmanagement/WorkFlexManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilegate.workflexmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlexManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
